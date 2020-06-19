e322d5abf53d994f4f8b36572d0089e857b34506
qicaisheng <qicaisheng@qq.com>
2020-06-19 17:54:12 +0800

    game can guess number

---
+++ src/main/java/com/twschool/practice/GuessNumberGame.java
@@ -0,0 +1,13 @@
+package com.twschool.practice;
+
+public class GuessNumberGame {
+    private GameAnswer gameAnswer;
+
+    public GuessNumberGame(GameAnswer gameAnswer) {
+        this.gameAnswer = gameAnswer;
+    }
+
+    public String guess(String userAnswerString) {
+        return gameAnswer.check(userAnswerString);
+    }
+}
---
+++ src/test/java/com/twschool/practice/GuessNumberGameTest.java
@@ -0,0 +1,26 @@
+package com.twschool.practice;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class GuessNumberGameTest {
+    @Test
+    public void should_return_4A0B_when_input_1234_given_game_with_answer_1234() {
+        GameAnswer gameAnswer = new GameAnswer("1 2 3 4");
+        GuessNumberGame guessNumberGame = new GuessNumberGame(gameAnswer);
+        
+        String result = guessNumberGame.guess("1 2 3 4");
+
+        Assert.assertEquals("4A0B", result);
+    }
+
+    @Test
+    public void should_return_2A0B_when_input_1256_given_game_with_answer_1234() {
+        GameAnswer gameAnswer = new GameAnswer("1 2 3 4");
+        GuessNumberGame guessNumberGame = new GuessNumberGame(gameAnswer);
+
+        String result = guessNumberGame.guess("1 2 5 6");
+
+        Assert.assertEquals("2A0B", result);
+    }
+}
