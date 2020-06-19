0e4a167e958c1da99c08dfadf21284f4b5f00110
qicaisheng <qicaisheng@qq.com>
2020-06-19 16:47:33 +0800

    should_return_4A0B_when_compare_with_1234_given_answer_1234

---
+++ src/main/java/com/twschool/practice/GameAnswer.java
@@ -0,0 +1,11 @@
+package com.twschool.practice;
+
+public class GameAnswer {
+    public GameAnswer(String answerString) {
+        
+    }
+
+    public String check(String userAnswerString) {
+        return "4A0B";
+    }
+}
