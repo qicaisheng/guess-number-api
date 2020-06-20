d4a802b45f9c6547146b9263183483ca15a20653
qicaisheng <qicaisheng@qq.com>
2020-06-20 11:21:38 +0800

    add main class

---
+++ src/main/java/com/twschool/practice/GuessNumberGameConsoleApp.java
@@ -0,0 +1,26 @@
+package com.twschool.practice;
+
+import java.util.Scanner;
+
+public class GuessNumberGameConsoleApp {
+    
+    public static void main(String[] args) {
+        GuessNumberGame guessNumberGame = new GuessNumberGame(new AnswerGenerator());
+        System.out.println("please guess number: eg, 1 2 3 4");
+        while (guessNumberGame.getStatus() == GameStatus.CONTINUED) {
+            Scanner scanner = new Scanner(System.in);
+            String userInput = scanner.nextLine();
+            
+            String result = guessNumberGame.guess(userInput);
+            
+            System.out.println(String.format("guess result: %s", result));
+        }
+        
+        if (guessNumberGame.getStatus() == GameStatus.SUCCEED) {
+            System.out.println("Congratulations, you guess succeed");
+        }
+        if (guessNumberGame.getStatus() == GameStatus.FAILED) {
+            System.out.println("Sorry, you guess failed");
+        }
+    }
+}
