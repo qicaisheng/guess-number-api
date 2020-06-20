5e57f3e42713394078e60b708b84063d6c6b54bf
qicaisheng <qicaisheng@qq.com>
2020-06-20 10:32:44 +0800

    rename test class

--- src/test/java/com/twschool/practice/GameAnswerTest.java
+++ src/test/java/com/twschool/practice/GameAnswerTest.java
@@ -3,7 +3,7 @@ package com.twschool.practice;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class AnswerTest {
+public class GameAnswerTest {
     @Test
     public void should_return_4A0B_when_compare_with_1234_given_answer_1234() {
         GameAnswer answer = new GameAnswer("1 2 3 4");
