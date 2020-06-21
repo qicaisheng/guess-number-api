02dda14e815a1c0bfef4d59f6e41945f6cd8d4eb
qicaisheng <qicaisheng@qq.com>
2020-06-21 11:59:05 +0800

    extract getContinuousSucceedCount method

--- src/main/java/com/twschool/practice/domain/GamePoints.java
+++ src/main/java/com/twschool/practice/domain/GamePoints.java
@@ -1,5 +1,6 @@
 package com.twschool.practice.domain;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,10 +14,15 @@ public class GamePoints {
     }
 
     public int totalPoints() {
+        List<Integer> list = getContinuousSucceedCount().stream().filter(count -> count > 3).collect(Collectors.toList());
+        return list.size() * 3;
+    }
+
+    private List<Integer> getContinuousSucceedCount() {
         Map<Integer, Integer> continuousSucceedCountMap = new HashMap<>();
         boolean firstRecordSucceed = gameRecords.get(0).getGameStatus() == GameStatus.SUCCEED;
         continuousSucceedCountMap.put(1, firstRecordSucceed ? 1 : 0);
-        
+
         for (int index = 1; index < gameRecords.size(); index++) {
             if (gameRecords.get(index -1).getGameStatus() != GameStatus.SUCCEED) {
                 continuousSucceedCountMap.put(continuousSucceedCountMap.size(), 0);
@@ -25,8 +31,7 @@ public class GamePoints {
                 continuousSucceedCountMap.put(index, continuousSucceedCountMap.get(index) + 1);
             }
         }
-        List<Integer> list = continuousSucceedCountMap.values().stream().filter(count -> count > 3).collect(Collectors.toList());
-        return list.size() * 3;
+        return new ArrayList<>(continuousSucceedCountMap.values());
     }
-    
+
 }
